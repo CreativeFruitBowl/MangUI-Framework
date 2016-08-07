46145c1cebabb9e4f549f50b1a23ecf01e59ab9f
Andi North <andi@mangopear.co.uk>
2016-08-07 23:16:28 +0100

    [Setup] Init new folder for Resource > Images.

---
+++ resources/images/_source/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Hidden page
+ *
+ * This file is in place simply to prevent people from being able to access 
+ * the directory from the web server. For your website's security, this file
+ * needs to remain in place.
+ *
+ * @package  	mangui
+ * @version  	1.0.0.alpha
+ * @since   	1.0.0.alpha
+ * @author  	Andi North <andi@mangopear.co.uk>
+ * @link 		https://mangopear.co.uk/mangui/
+ * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+ */
+	
+?>
