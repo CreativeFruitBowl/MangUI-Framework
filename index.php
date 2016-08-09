5f3fbfe53634aee0f9971b1473469a0c868007cd
Andi North <andi@mangopear.co.uk>
2016-08-09 20:09:20 +0100

    [Setup] Init *very* basic index template so that the theme shows in WordPress Dashboard and front end.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * [Core WordPress Template] Index
+ *
+ * The index template is the default template WordPress uses for the homepage - unless a 
+ * home.php template exists within the theme.
+ *
+ * The index template is also the default template used for listing posts - for instance
+ * a blog index.
+ *
+ * @package     mangui
+ * @category    core-templates
+ * @since       1.0.0.alpha.4
+ * @version     1.0.0.alpha.4
+ * @author      Andi North <andi@mangopear.co.uk>
+ * @link        https://mangopear.co.uk/mangui/
+ * @license     GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+ */
+
+
+/**
+ * CHANGELOG
+ *
+ * @version 1.0.0.alpha.4
+ *          Init file
+ *          Add header and footer calls
+ */
+
+get_header();
+
+get_footer();
