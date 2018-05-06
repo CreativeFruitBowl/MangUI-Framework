16464cb56adcf19c18aba59573be4e5faad22695
Andi North <andi@mangopear.co.uk>
2018-05-06 23:08:01 +0100

    Fixed issues with gulp not working

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,122 @@
 /**
- * Processing of our styles and scripts for performance and workflow
+ * Register custom post type for Hildon website: Frequently asked questions (for the general website) [faqs]
  *
- * This is the default MangUI Gulp process for concatenating and minifying the SASS
- * and Javascript files within the build.
+ * This post type holds the FAQs for general website use, they appear on the help page.
  *
- * @package  	mangui
- * @version  	1.0.0
- * @since   	1.0.0.alpha
+ * @category 	Compilation
+ * @package  	hildon
  * @author  	Andi North <andi@mangopear.co.uk>
- * @link 		https://mangopear.co.uk/mangui/
+ * @copyright  	2015 Mangopear creative
  * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+ * @version  	1.10.0
+ * @since   	1.0.0
  */
 
 
 /**
-* Contents
-*
-* [1]	Load plugins
-* [2]	Styles
-* [3]	Plugins
-* [4]	Scripts
-* [5]	Images
-* [6]	Watch
-* [7]	Task
-*/
-
-
-/**
- * [1]	Load plugins
- */
- 
-var gulp = require('gulp'),
- 	plugins = require('gulp-load-plugins')({ camelize: true }),
-	lr = require('tiny-lr'),
-	server = lr();
-
-
-/**
- * [2]	Styles
- */
-
-gulp.task('styles', function() {
-	return gulp.src('resources/css/sass/*.scss')
-		.pipe(plugins.rubySass({ style: 'expanded', sourcemap: true }))
-		.pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
-		.pipe(gulp.dest('resources/css/compiled'))
-		.pipe(plugins.notify({ message: 'Your SCSS files have been processed and minified, ready for deployment.' }));
-});
-
-
-/**
- * [3]	Plugins
- */
-
-gulp.task('plugins', function() {
-	return gulp.src(['resources/js/source/plugins.js', 'resources/js/vendor/*.js'])
-		.pipe(plugins.concat('plugins.js'))
-		.pipe(gulp.dest('resources/js/compiled'))
-		.pipe(plugins.rename({ suffix: '.min' }))
-		.pipe(plugins.uglify())
-		.pipe(gulp.dest('resources/js/compiled'))
-		.pipe(plugins.notify({ message: 'Your JavaScript plugin files have been processed, ready for deployment.' }));
-});
-
-
-/**
- * [4]	Scripts
- */
-
-gulp.task('scripts', function() {
-	return gulp.src(['resources/js/source/*.js', '!resources/js/source/plugins.js'])
-		.pipe(plugins.jshint('.jshintrc'))
-		.pipe(plugins.jshint.reporter('default'))
-		.pipe(plugins.concat('global.js'))
-		.pipe(gulp.dest('resources/js/compiled'))
-		.pipe(plugins.rename({ suffix: '.min' }))
-		.pipe(plugins.uglify())
-		.pipe(gulp.dest('resources/js/compiled'))
-		.pipe(plugins.notify({ message: 'Your JavaScript source files have been processed, ready for deployment.' }));
-});
-
-
-/**
- * [5]	Images
- */
-
-gulp.task('images', function() {
-	return gulp.src('resources/images/**/*', '!resources/images/**/*.psd')
-		.pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 7, progressive: true, interlaced: true })))
-		.pipe(gulp.dest('resources/images'))
-		.pipe(plugins.notify({ message: 'Your images have been minified, ready for deployment.' }));
-});
-
-
-/**
- * [6]	Watch
+ * Contents
  *
- * 		[a]	Watch on port 35729
- * 		[b]	Watch .scss files
- * 		[c]	Watch .js files
- * 		[d]	Watch image files
- */
-
-gulp.task('watch', function() {
-	gulp.watch('resources/css/sass/**/*.scss', ['styles']); // [b]
-	gulp.watch('resources/js/plugins/*.js', ['plugins']); // [c]
-	gulp.watch('resources/js/source/*.js', ['scripts']); // [c]
-	gulp.watch('resources/images/**/*', ['images']); // [d]
-});
-
-
-/**
+ * [1]	Load plugins
+ * [2]	Styles
+ * [3]	Plugins
+ * [4]	Scripts
+ * [5]	Images
+ * [6]	Watch
  * [7]	Task
  */
 
-gulp.task('default', ['styles', 'plugins', 'scripts', 'watch']);
+
+	/**
+ 	 * [1]	Load plugins
+ 	 */
+ 	
+ 	const sass = require('gulp-ruby-sass');
+	 
+	var gulp = require('gulp'),
+	    notify = require("gulp-notify"),
+	 	plugins = require('gulp-load-plugins')({ camelize: true }),
+		lr = require('tiny-lr'),
+		server = lr();
+
+
+	/**
+	 * [2]	Styles
+	 */
+	
+	gulp.task('styles', function() {
+		return sass('resources/css/sass/*.scss', {
+			style: 'compressed'
+		})
+			.on('error', sass.logError)
+			.pipe(plugins.autoprefixer('last 2 versions', 'ie 10', 'ios 6', 'android 4'))
+			.pipe(gulp.dest('resources/css/compiled'))
+			.pipe(plugins.notify({ title: 'MangUI', message: 'Your SASS and CSS has been processed.' }));
+	});
+
+
+	/**
+	 * [3]	Plugins
+	 */
+	
+	gulp.task('plugins', function() {
+		return gulp.src(['resources/js/source/plugins.js', 'resources/js/vendor/*.js'])
+			.pipe(plugins.concat('plugins.js'))
+			.pipe(gulp.dest('resources/js/compiled'))
+			.pipe(plugins.rename({ suffix: '.min' }))
+			.pipe(plugins.uglify())
+			.pipe(gulp.dest('resources/js/compiled'))
+			.pipe(plugins.notify({ title: 'MangUI', message: 'Your JavaScript plugins have been processed.' }));
+	});
+
+
+	/**
+	 * [4]	Scripts
+	 */
+	
+	gulp.task('scripts', function() {
+		return gulp.src(['resources/js/source/*.js', '!resources/js/source/plugins.js'])
+			.pipe(plugins.jshint('.jshintrc'))
+			.pipe(plugins.jshint.reporter('default'))
+			.pipe(plugins.concat('global.js'))
+			.pipe(gulp.dest('resources/js/compiled'))
+			.pipe(plugins.rename({ suffix: '.min' }))
+			.pipe(plugins.uglify())
+			.pipe(gulp.dest('resources/js/compiled'))
+			.pipe(plugins.notify({ title: 'MangUI', message: 'Your JavaScript scripts have been processed.' }));
+		});
+
+
+	/**
+	 * [5]	Images
+	 */
+	
+	gulp.task('images', function() {
+		return gulp.src('resources/images/**/*', '!resources/images/**/*.psd')
+			.pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 7, progressive: true, interlaced: true })))
+			.pipe(gulp.dest('resources/images'))
+			.pipe(plugins.notify({ title: 'MangUI', message: 'Your images have been processed.' }));
+	});
+
+
+	/**
+	 * [6]	Watch
+	 *
+	 * 		[a]	Watch on port 35729
+	 * 		[b]	Watch .scss files
+	 * 		[c]	Watch .js files
+	 * 		[d]	Watch image files
+	 */
+	
+	gulp.task('watch', function() {
+		gulp.watch('resources/css/sass/**/*.scss', ['styles']); // [b]
+		gulp.watch('resources/js/vendor/*.js', ['plugins']); // [c]
+		gulp.watch('resources/js/source/*.js', ['scripts']); // [c]
+		gulp.watch('resources/images/**/*', ['images']); // [d]
+	});
+
+
+	/**
+	 * [7]	Task
+	 */
+	
+	gulp.task('default', ['styles', 'plugins', 'scripts', 'watch']);
